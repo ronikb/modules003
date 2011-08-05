29d6994637f34d5f1c1c9aeac213a7cfa7e08e8a
Roni <roni@citrusinformatics.com>
2011-08-05 13:21:25 +0530

    DELETED UNWANTED MANIFESTS

--- application/manifests/dbcreate.pp
+++
@@ -1,8 +0,0 @@
-# This will create MySql database 
-
-class application::dbcreate {
-	exec { "db-create":
-		command =>"/etc/puppet/modules/application/scripts/mysql-db-create.sh $application_mysql_dbname",
-		require => Service["mysql"]
-}
-}
--- application/manifests/dbrestore.pp
+++
@@ -1,8 +0,0 @@
-#This will restore MySql database 
-class application::dbrestore {
-	exec { "db-restore":
-		command =>"/etc/puppet/modules/application/scripts/mysql-db-restore.sh $application_mysql_dbname $application_mysql_dump_location_for_dbrestore",
-		require => Class["application::dbcreate"],
-}
-}
-
