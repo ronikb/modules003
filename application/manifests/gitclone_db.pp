ef34c23ca8a95890cba497c5f41606a753c6d07d
Roni <roni@citrusinformatics.com>
2011-08-05 12:34:32 +0530

    mergeg application manifests

--- application/manifests/gitclone_db.pp
+++ application/manifests/gitclone_db.pp
@@ -7,6 +7,26 @@ class application::gitclone_db {
         logoutput=> on_failure, 
 		before => Class ["application::dbrestore"]
 }
+	exec { "gitclone-application":
+		command => "git clone $application_gitclone_application $application_gitclone_application_destination",
+		require => Exec ["gitclone-db"]
+}
+	exec { "db-create":
+		command =>"/etc/puppet/modules/application/scripts/mysql-db-create.sh $application_mysql_dbname",
+		require => Service["mysql"]
+}
+	exec { "db-restore":
+		command =>"/etc/puppet/modules/application/scripts/mysql-db-restore.sh $application_mysql_dbname $application_mysql_dump_location_for_dbrestore",
+		require => Exec["db-create"],
+}
+	 exec { "increase-php-memory-limit":
+			command => "sed -i 's/memory_limit = .*/memory_limit = $application_php_memory_limit/' /etc/php5/apache2/php.ini",
+        	require => Package["php5"]
+}
+	exec { "symlink-for-files-folder":
+		command => "ln -s $application_symlink_files_folder_source $application_symlink_files_folder_destination",
+		require => Exec["gitclone-application"],
+}
 }
 
 
